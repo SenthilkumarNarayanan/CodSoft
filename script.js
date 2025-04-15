0cd41b81c068cf3ffdf53dd2326ec7841838f3db
SenthilkumarNarayanan <124486793+SenthilkumarNarayanan@users.noreply.github.com>
2025-04-16 05:07:21 +0530

    caculator files added

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const userInput= document.getElementById("input");
+var expression= "";
+
+function press(num){
+    expression += num;
+    input.value= expression;
+}
+
+function equal(){
+    input.value=eval(expression);
+    expression="";
+
+}
+function clear(){
+    expression="";
+    userInput.value=expression
+}
